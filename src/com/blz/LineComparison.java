2f0e6c8d3597db333da689dcbe6812a0b37cbd89
Priyanka <priyankadombale27@gmail.com>
2022-02-17 16:30:01 +0530

    Added: check equality of two line logic

--- src/com/blz/LineComparison.java
+++ src/com/blz/LineComparison.java
@@ -7,17 +7,22 @@ public class LineComparison {
 
 		// Declare and Initialize variable (x,y)coordinates.
 
-		int x1 = 2;
-		int y1 = 7;
-		int x2 = 5;
-		int y2 = 3;
+		int x1 = 2, y1 = 7, x2 = 5, y2 = 3;
+		int x11 = 2, y11 = 7, x12 = 5, y12 = 3;
 
 		// Calculate length of line using given formula.
 
-		double Lengthofline = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		double Lengthofline1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		double Lengthofline2 = Math.sqrt(Math.pow((x12 - x11), 2) + Math.pow((y12 - y11), 2));
+
 		System.out.println(
-				"Length of the line" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") :" + Lengthofline);
+				"Length of the line" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") :" + Lengthofline1);
+		System.out.println(
+				"Length of the line" + "(" + x11 + "," + y11 + ")," + "(" + x12 + "," + y12 + ") :" + Lengthofline2);
 
+		if (Lengthofline1 == Lengthofline2)
+			System.out.println("Both Two lines are equal");
+		else
+			System.out.println("Both Two lines are not equal");
 	}
-
 }
