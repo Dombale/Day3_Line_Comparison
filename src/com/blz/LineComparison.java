14eaa0d867c4ba55cd689f3b8a108b19620de4ee
Priyanka <priyankadombale27@gmail.com>
2022-02-17 15:49:39 +0530

    Added: Calculate length of line logic

--- src/com/blz/LineComparison.java
+++ src/com/blz/LineComparison.java
@@ -5,6 +5,15 @@ public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome in Line Comparison..!!!");
 
+		int x1 = 2;
+		int y1 = 7;
+		int x2 = 5;
+		int y2 = 3;
+
+		double Lengthofline = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		System.out.println(
+				"Length of the line" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") :" + Lengthofline);
+
 	}
 
 }
